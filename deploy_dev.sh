2700a705ec85d63ca237cbc04abb336cb8b18256
Anoopg-exp <anoop.g@experionglobal.com>
2016-03-04 18:59:04 +0530

    added

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -16,7 +16,7 @@ echo "Uploading to HockeyApp..."
 curl \
 -F "status=2" \
 -F "notify=0" \
--F "ipa=@app\build\outputs\apk\app-production-debug.apk" \
+-F "ipa=@home\ubuntu\DBFlowTest\app\build\outputs\apk\app-production-debug.apk" \
 -H "X-HockeyAppToken: $HOCKEYAPP_TOKEN" \
 https://rink.hockeyapp.net/api/2/apps/cb0a76aee7bd452aa671551e7a028efb/app_versions/upload
 fi
