1b12ed46b5bff6acc73d13378104d04c75b24c9d
Anoopg-exp <anoop.g@experionglobal.com>
2016-03-04 18:52:20 +0530

    unexpected end fixed

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -19,3 +19,4 @@ curl \
 -F "ipa=@app\build\outputs\apk\app-production-debug.apk" \
 -H "X-HockeyAppToken: $HOCKEYAPP_TOKEN" \
 https://rink.hockeyapp.net/api/2/apps/cb0a76aee7bd452aa671551e7a028efb/app_versions/upload
+fi
