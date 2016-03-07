3be1b39d93c1fc6e301e10cdb0c773fd880c2faa
Anoopg-exp <anoop.g@experionglobal.com>
2016-03-07 11:36:34 +0530

    chaned checking statement

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -7,16 +7,16 @@ if [ "$1" ]
 then
 NOTES="$1"
 fi
-if [ ! ­f "app/build/outputs/apk/app­debug.apk" ]
+if [ ! ­f "app/build/outputs/apk/app-production-debug.apk" ]
 then
-echo "app/build/outputs/apk/app­debug.apk not found!"
+echo "app/build/outputs/apk/app-production-debug.apk not found!"
 else
 echo "Uploading to HockeyApp..."
 
 curl \
 -F "status=2" \
 -F "notify=0" \
--F "ipa=@app\build\outputs\apk\app­debug.apk" \
+-F "ipa=@DBFlowTest/app/build/outputs/apk/app-production-debug.apk" \
 -H "X-HockeyAppToken: $HOCKEYAPP_TOKEN" \
 https://rink.hockeyapp.net/api/2/apps/cb0a76aee7bd452aa671551e7a028efb/app_versions/upload
 fi
