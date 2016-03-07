867312dd29a37795569ceb6276980e899323fd18
Anoopg-exp <anoop.g@experionglobal.com>
2016-03-07 12:19:32 +0530

    changed conditional statements

--- deploy_tohockey.sh
+++ deploy_tohockey.sh
@@ -10,13 +10,14 @@ fi
 if [ ! ­f "app/build/outputs/apkapp-production-debug.apk" ]
 then
 echo "app/build/outputs/apkapp-production-debug.apk not found!"
-else
-echo "Uploading to HockeyApp..."
-
 curl \
 -F "status=2" \
 -F "notify=0" \
 -F "ipa=@DBFlowTest/app/build/outputs/apk/app-production-debug.apk" \
 -H "X-HockeyAppToken: $HOCKEYAPP_TOKEN" \
 https://rink.hockeyapp.net/api/2/apps/cb0a76aee7bd452aa671551e7a028efb/app_versions/upload
+else
+echo "Uploading to HockeyApp..."
+
+
 fi
